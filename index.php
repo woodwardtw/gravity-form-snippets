5b0bae0f01b52be7a657215587cfbc75c0694910
Tom Woodward <twwoodward@me.com>
2017-11-22 06:16:07 -0500

    init
    Gravity Forms Snippets that have been useful

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+//gives you a clean URL to files uploaded via Gravity Forms so that building links or embedding them via the URL works
+add_filter( 'gform_secure_file_download_location', '__return_false' );
+
+
+/*
+***
+FORM SUBMISSION TO POST RELATED STUFF
+***
+*/
+
+//takes submitter to the post create on submissions that create posts
+add_action('gform_after_submission', 'redirect_on_post', 10, 2);
+function redirect_on_post($entry, $form) {
+    $post_id = $entry['post_id'];
+    $url = get_site_url() . "/?p=" . $post_id;
+    wp_redirect($url);
+    exit;
+}
+
+
+//set post password on submissions that create posts --- I think it uses the first field as the password but it's been a while
+add_action( 'gform_after_submission', 'set_default_values', 10, 2 );
+function set_default_values( $entry, $post ) {		
+      		
+      		$post = get_post( $entry['post_id'] );
+      		$pw = $entry[1]; //
+
+               wp_update_post(array(
+            	'ID' => $post->ID,
+            	'post_password' => $pw,
+        ));
+}
+
+
